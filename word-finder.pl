ea06fa5d9393220b6de5075ee47ef60766025304
Michael Jeung <jeungster@gmail.com>
2012-04-29 14:53:22 -0700

    accept user input

--- word-finder.pl
+++ word-finder.pl
@@ -1,5 +1,19 @@
 #!/usr/bin/perl
 
-print "Input a string of numbers: ";
+print "\n" .
+      "This script takes a string of numbers and prints a list \n" .
+      "of valid words that can be created with those numbers.\n" .
+      "\n";
 
+print "Input: ";
 
+chomp($user_input = <>);
+
+if ( $user_input =~ m/^[0-9]+$/ )
+{
+  print $user_input . "\n";
+}
+else
+{
+  print "\nMalformed input \"$user_input\", must be digits only\n\n";
+}
