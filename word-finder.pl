e12e480fd0d7938f5d9d0472e29992e26a2d4c75
Michael Jeung <jeungster@gmail.com>
2012-04-29 16:14:22 -0700

    working on findPossiblWords

--- word-finder.pl
+++ word-finder.pl
@@ -1,4 +1,56 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+
+use Switch;
+
+sub translateNumber 
+{
+  my ($inputNumber) = @_;
+
+  my @outputArray;
+  switch ($inputNumber) 
+  {
+    case(2) { @outputArray = ("a","b","c"); }
+    case(3) { @outputArray = ("d","e","f"); }
+    case(4) { @outputArray = ("g","h","i"); }
+    case(5) { @outputArray = ("j","k","l"); }
+    case(6) { @outputArray = ("m","n","o"); }
+    case(7) { @outputArray = ("p","q","r"); }
+    case(8) { @outputArray = ("t","u","v"); }
+    case(9) { @outputArray = ("w","x","y"); }
+    else    { @outputArray = (); }
+  }
+
+  return (@outputArray);
+}
+
+sub findPossibleWords
+{
+  my (@remainingDigits, @wordSoFar) = @_;
+
+  print "Entering function with r: @remainingDigits, w: @wordSoFar";
+
+  if (scalar(@remainingDigits) == -1)
+  {
+     print "word: @wordSoFar\n";
+  }
+  else
+  {
+    my $nextDigit = $remainingDigits[0];
+    print scalar(@remainingDigits);
+    shift(@remainingDigits);
+    print scalar(@remainingDigits);
+    my @translationResult = translateNumber($nextDigit);
+    foreach my $item (@translationResult)
+    {
+      my @localWord = @wordSoFar;
+      push(@localWord,$item);
+      print "localWord: @localWord\n";
+      findPossibleWords(@remainingDigits,@localWord);
+    }
+  }
+}
 
 print "\n" .
       "This script takes a string of numbers and prints a list \n" .
@@ -7,13 +59,19 @@ print "\n" .
 
 print "Input: ";
 
-chomp($user_input = <>);
+chomp(my $userInput = <>);
 
-if ( $user_input =~ m/^[0-9]+$/ )
+if ( $userInput =~ m/^[0-9]+$/ )
 {
-  print $user_input . "\n";
+  my @charArray = split('',$userInput);
+
+  print "@charArray\n";
+
+  my @progress = ();
+
+  findPossibleWords(@charArray,@progress);
 }
 else
 {
-  print "\nMalformed input \"$user_input\", must be digits only\n\n";
+  print "\nMalformed input \"$userInput\", must be digits only\n\n";
 }
