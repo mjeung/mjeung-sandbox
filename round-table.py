74ab14410c1cb81bff3c0f4fe19676dbe1aff0c0
Michael Jeung <jeungster@gmail.com>
2012-08-05 17:32:56 -0700

    start of round table problem

---
+++ round-table.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+##################################################
+
+class RoundTable(object):
+
+  def arrangements(self, countA, countB, chairs, minDistance):
+    print "countA: ", countA
+    print "countB: ", countA
+    print "chairs: ", chairs
+    print "minDistance: ", minDistance
+
+
+
+countA = 1
+countB = 1
+chairs = 10
+minDistance = 1
+roundTable = RoundTable()
+roundTable.arrangements(countA, countB, chairs, minDistance);
