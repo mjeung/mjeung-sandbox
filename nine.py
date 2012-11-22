8180f1bc87641ce59621cff71ef01343b7081b0e
Michael Jeung <jeungster@gmail.com>
2012-11-22 11:59:01 -0800

    added stuff

--- nine.py
+++ nine.py
@@ -18,12 +18,16 @@ assert digitalRoot([1,2,3]) == 6
 assert digitalRoot([1,2,3,4,5]) == 6
 assert digitalRoot([1,2,3,4,5,6,7,8]) == 9
 
-doorNumber = 9
 
-for combo in list(itertools.combinations('1345678',5)):
+
+doorNumber = 9
+numberOfPeople = 5
+people = '1345678'
+
+for combo in list(itertools.combinations(people,numberOfPeople)):
 	if digitalRoot(map(int,combo)) == doorNumber:
 		print map(int,combo), " go into door ", doorNumber
-		remaining = map(int,set(['1','3','4','5','6','7','8']) - set(combo) )
+		remaining = map(int,set(list(people)) - set(combo) )
 		print remaining, "can go into door ", digitalRoot(remaining)
 		print "*******"
 
