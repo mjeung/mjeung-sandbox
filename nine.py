4b0856541632cf8fd3f9692ab28072109bd442d2
Michael Jeung <jeungster@gmail.com>
2012-11-22 13:05:40 -0800

    I just made changes to this script`

--- nine.py
+++ nine.py
@@ -18,16 +18,20 @@ assert digitalRoot([1,2,3]) == 6
 assert digitalRoot([1,2,3,4,5]) == 6
 assert digitalRoot([1,2,3,4,5,6,7,8]) == 9
 
-
-
 doorNumber = 9
-numberOfPeople = 5
+numberOfPeople = 4
 people = '1345678'
 
 for combo in list(itertools.combinations(people,numberOfPeople)):
 	if digitalRoot(map(int,combo)) == doorNumber:
-		print map(int,combo), " go into door ", doorNumber
-		remaining = map(int,set(list(people)) - set(combo) )
-		print remaining, "can go into door ", digitalRoot(remaining)
-		print "*******"
+		print ""
+		print "*****************"
+		print map(int,combo), " can go into door", doorNumber
+		remaining = map(int, set(list(people)) - set(combo) )
+                if len(remaining) >= 3:
+			print remaining, "can go into door ", digitalRoot(remaining)
+		else:
+			print remaining, "are stuck" 
 
+
+print ""
