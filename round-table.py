04eaf71dccc4e1cb26bd7bbd574df2e9ed2aa012
Michael Jeung <jeungster@gmail.com>
2012-11-21 16:22:18 -0800

    add roundtable

--- round-table.py
+++ round-table.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 
+class Employee(object):
+  def __init__(self, company, id):
+    self.myCompany = company
+    self.myId = id
+
+  def display(self):
+    print self.myCompany, "employee, id: ", self.myId
+
+  myCompany = ""
+  myId = 0
+
+##################################################
+class EmployeeGenerator(object):
+  def makeEmployee(self, company):
+    self.counter = self.counter + 1
+    return Employee(company,self.counter)
+
+  counter = 0
+  
+
 ##################################################
 
 class RoundTable(object):
@@ -10,6 +30,25 @@ class RoundTable(object):
     print "chairs: ", chairs
     print "minDistance: ", minDistance
 
+    # Build Table
+    for i in range(0,chairs):
+      self.table.append(0)
+
+    # Build list of employees
+    for i in range(0,countA):
+      self.listOfEmployees.append(employeeGenerator.makeEmployee("Phoenix"))
+    for i in range(0,countB):
+      self.listOfEmployees.append(employeeGenerator.makeEmployee("Corsair"))
+
+    print self.table
+
+    return 10;
+
+  table = []
+  listOfEmployees = []
+  employeeGenerator = EmployeeGenerator()
+
+#  def recursiveFindSolution(table): 
 
 
 countA = 1
@@ -17,4 +56,7 @@ countB = 1
 chairs = 10
 minDistance = 1
 roundTable = RoundTable()
-roundTable.arrangements(countA, countB, chairs, minDistance);
+returnValue = roundTable.arrangements(countA, countB, chairs, minDistance);
+ 
+print "returnValue: ", returnValue
+
